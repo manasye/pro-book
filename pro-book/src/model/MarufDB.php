1e6d6f4fd52c12f39d763e30b9420b7614b40c13
Ahmad Izzan <aahmadizzan@gmail.com>
2018-11-27 17:29:54 +0700

    change probook database structure

--- pro-book/src/model/MarufDB.php
+++ pro-book/src/model/MarufDB.php
@@ -16,7 +16,10 @@ class MarufDB {
 
   private function Connect() {
     try {
-      $this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->dbName, $this->dbUser, $this->dbPassword);
+      // echo $this->dbUser;
+      // echo $this->dbPassword;
+      // $this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->dbName, $this->dbUser, $this->dbPassword);
+      $this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->dbName, $this->dbUser, '');
     } catch (PDOException $e) {
       print "Error!: " . $e->getMessage() . "<br/>";
       die();
@@ -70,7 +73,7 @@ class MarufDB {
   }
 
   public function searchBook($title) {
-    $query = $this->pdo->prepare("SELECT * FROM Books WHERE LOWER(title) LIKE LOWER(?)");
+    $query = $this->pdo->prepare("SELECT * FROM Ratings WHERE LOWER(title) LIKE LOWER(?)");
     $query->execute(array("%{$title}%"));
     return $query->fetchAll();
   }
@@ -151,7 +154,7 @@ class MarufDB {
   }
 
   public function getHistory($user_id) {
-    $query = $this->pdo->prepare("SELECT Orders.id as order_id, Books.id as book_id, order_timestamp, is_review, title, amount  FROM Orders JOIN Books ON Orders.book_id = Books.id WHERE user_id = ? ORDER BY Orders.id DESC");
+    $query = $this->pdo->prepare("SELECT Orders.id as order_id, Ratings.id as book_id, order_timestamp, is_review, title, amount  FROM Orders JOIN Ratings ON Orders.book_id = Ratings.id WHERE user_id = ? ORDER BY Orders.id DESC");
     $query->execute(array($user_id));
     return $query->fetchAll();
   }
@@ -163,7 +166,7 @@ class MarufDB {
   }
 
   public function getBookDetail($book_id) {
-    $query = $this->pdo->prepare("SELECT * FROM Books WHERE id = ?");
+    $query = $this->pdo->prepare("SELECT * FROM Ratings WHERE id = ?");
     $query->execute(array($book_id));
     return $query->fetch();
   }
@@ -172,12 +175,12 @@ class MarufDB {
     try {
       $query = $this->pdo->prepare("INSERT INTO Reviews (username, book_id, rating, comment, user_id) VALUES (?, ?, ?, ?, ?)");
       $query->execute(array($username, $book_id, $rating, $comment, $user_id));
-      $query = $this->pdo->prepare("SELECT * FROM Books WHERE id = ?");
+      $query = $this->pdo->prepare("SELECT * FROM Ratings WHERE id = ?");
       $query->execute(array($book_id));
       $result = $query->fetch();
       $currVote = $result['vote'] + 1;
       $currRating = ($result['rating'] * $result['vote'] + $rating) / $currVote;
-      $query = $this->pdo->prepare("UPDATE Books SET rating = ?, vote = ? WHERE id = ?");
+      $query = $this->pdo->prepare("UPDATE Ratings SET rating = ?, vote = ? WHERE id = ?");
       $query->execute(array($currRating, $currVote, $book_id));
       $query = $this->pdo->prepare("UPDATE Orders SET is_review = 1 WHERE id = ?");
       $query->execute(array($order_id));
