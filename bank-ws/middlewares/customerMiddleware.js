7518dd79b4933c159d289693b6c80b6e63fa6bb7
manasye <manase32@gmail.com>
2018-11-30 19:41:51 +0700

    fix bug in customer middleware [Node]

--- bank-ws/middlewares/customerMiddleware.js
+++ bank-ws/middlewares/customerMiddleware.js
@@ -1,21 +1,21 @@
-Customer = require("../models/customer");
+Customer = require('../models/customer');
 
 module.exports = function(req, res, next) {
     console.log(req.body);
     Customer.getByCardNumber(req.body.cardNumber)
         .then(customer => {
-            if (customer == null) {
+            if (customer !== null) {
                 req.body.customerAccount = customer;
                 next();
             } else {
-                throw new Error("Account number invalid");
+                throw new Error('Account number invalid');
             }
         })
         .catch(error => {
-            console.log("[ERROR]: " + error.message);
+            console.log('[ERROR]: ' + error.message);
             res.json({
                 success: false,
-                message: "Account Number invalid"
+                message: 'Account Number invalid'
             });
         });
 };
